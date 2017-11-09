06868dca933469bf0f3171c34a32f620770bc334
Salam Dawood <salamd135@gmail.com>
2017-11-09 17:21:59 -0500

    initial commit

---
+++ MyCare/app/src/main/java/edu/csi5230/salamdawood/mycare/my_info.java
@@ -0,0 +1,13 @@
+package edu.csi5230.salamdawood.mycare;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class my_info extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_info);
+    }
+}
